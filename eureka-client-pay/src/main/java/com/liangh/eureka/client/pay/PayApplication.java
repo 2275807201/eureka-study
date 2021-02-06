e9bee5d4e8c6e8abc2de4876fd6943d1b24591b0
lianghuan <2275807201@qq.com>
2021-02-06 09:36:45 +0800

    eureka-client-pay 搭建

---
+++ eureka-client-pay/src/main/java/com/liangh/eureka/client/pay/PayApplication.java
@@ -0,0 +1,21 @@
+package com.liangh.eureka.client.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @version V1.0
+ * @description:
+ * @author: liangh
+ * @create: 2020/12/25 09:28
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class, args);
+    }
+
+}
